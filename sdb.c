79f3e9586d9ac7b347fc7d8a1bd8bffb0f5b7ec2
Koji OGOCHI <ogochi@jamstec.go.jp>
2010-05-25 07:31:08 +0000

    first version

---
+++ sdb.c
@@ -0,0 +1,88 @@
+/*
+ * sdb.c -- simple database (plain text).
+ */
+#include <string.h>
+#include <stdio.h>
+
+#include "internal.h"
+#include "logging.h"
+
+
+static FILE *db = NULL;
+
+
+int
+sdb_free()
+{
+    int rval = 0;
+
+    if (db)
+        rval = fclose(db);
+
+    db = NULL;
+    return rval;
+}
+
+
+int
+sdb_open(const char *path)
+{
+    sdb_free();
+
+    if ((db = fopen(path, "r")) == NULL) {
+        logging(LOG_SYSERR, path);
+        return -1;
+    }
+    return 0;
+}
+
+
+char *
+sdb_readitem(const char *item)
+{
+    char aline[4096];
+    char *ptr;
+    char *value = NULL;
+
+    if (!db)
+        return NULL;
+
+    rewind(db);
+    while (!feof(db)) {
+        read_logicline(aline, sizeof aline, db);
+
+        /* skip a comment line or a blank line */
+        if (aline[0] == '#' || aline[0] == '\0')
+            continue;
+
+        for (ptr = aline;
+             *ptr != '\0' && *ptr != ' ' && *ptr != '\t';
+             ptr++)
+            ;
+
+        if (strncmp(aline, item, ptr - aline) == 0) {
+            while (*ptr == ' ' || *ptr == '\t')
+                ptr++;
+
+            value = strdup(ptr);
+            if (value == NULL)
+                logging(LOG_SYSERR, NULL);
+            break;
+        }
+    }
+    return value;
+}
+
+
+#if 0
+int
+test_sdb(void)
+{
+    sdb_open("conf/20c3m.conf");
+
+    printf("(%s)\n", sdb_readitem("comment"));
+
+    sdb_free();
+    return 0;
+}
+#endif
